30081512c8e2d974daa049ffe4b1385a1ae0d649
DrAg0n141 <44865095+drag0n141@users.noreply.github.com>
2024-02-14 14:02:48 +0100

    Update settings.js

--- kubernetes/main/apps/home/node-red/app/config/settings.js
+++ kubernetes/main/apps/home/node-red/app/config/settings.js
@@ -3,6 +3,32 @@ module.exports = {
   credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET,
   flowFilePretty: true,
 
+  adminAuth: {
+    type: "strategy",
+    strategy: {
+      name: "openidconnect",
+      autoLogin: true,
+      label: "Sign in",
+      icon: "fa-cloud",
+      strategy: require("passport-openidconnect").Strategy,
+      options: {
+        issuer: "https://authelia.${SECRET_DOMAIN}",
+        authorizationURL: "https://authelia.${SECRET_DOMAIN}/api/oidc/authorization",
+        tokenURL: "http://authelia.security.svc.cluster.local:9091/api/oidc/token",
+        userInfoURL: "http://authelia.security.svc.cluster.local:9091/api/oidc/userinfo",
+        clientID: "nodered",
+        clientSecret: process.env.NODE_RED_OAUTH_CLIENT_SECRET,
+        callbackURL: "https://nodered.${SECRET_DOMAIN_INTERNAL}/auth/strategy/callback",
+        scope: ["email", "profile", "openid"],
+        proxy: true,
+        verify: function (issuer, profile, done) {
+          done(null, profile)
+        },
+      },
+    },
+    users: [{username: "drag0n141", permissions: ["*"]}],
+  },
+
   uiPort: process.env.PORT || 1880,
 
   diagnostics: {
