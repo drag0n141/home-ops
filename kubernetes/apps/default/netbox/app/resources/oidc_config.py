085446fdddae58330b1d9db32cfceacb4de7b64a
DrAg0n141 <social.github@d141.de>
2026-04-24 22:24:00 +0200

    fix: try this way

---
+++ kubernetes/apps/default/netbox/app/resources/oidc_config.py
@@ -0,0 +1,21 @@
+from os import environ
+
+SOCIAL_AUTH_OIDC_ENDPOINT = environ.get('SOCIAL_AUTH_OIDC_ENDPOINT')
+SOCIAL_AUTH_OIDC_KEY = environ.get('SOCIAL_AUTH_OIDC_KEY')
+SOCIAL_AUTH_OIDC_SECRET = environ.get('SOCIAL_AUTH_OIDC_SECRET')
+SOCIAL_AUTH_OIDC_SCOPE = ["openid", "profile", "email", "groups"]
+SOCIAL_AUTH_PIPELINE = (
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.social_auth.associate_by_email",
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+    "netbox.sso_pipeline_roles.add_groups",
+    "netbox.sso_pipeline_roles.remove_groups",
+    "netbox.sso_pipeline_roles.set_roles",
+)
